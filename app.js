5d40298e23b1a5ee794e36d1c7f74c4a456a08d7
Oscar Moran <oscar91moran@gmail.com>
2024-01-11 14:30:44 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let age = 15;
+let movieRating = "PG";
+
+// if (age > 17) console.log("You can watch any movie!");
+// else if (age === 17) console.log("You need more than 17 years old!");
+// else console.log("you are younger to see movie");
+
+if (age > 17) {
+  console.log("You can watch any movie!");
+} else if (age === 17) {
+  console.log("You need more than 17 years old!");
+} else {
+  console.log("You are younger to see the movie");
+}
